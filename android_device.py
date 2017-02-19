02c04fde408e8dab55498df8c33abab11968021a
johnsayeau <ugl539pal405>
2017-02-19 14:23:19 -0700

    extended device class

--- android_device.py
+++ android_device.py
@@ -9,10 +9,16 @@ def run_cmd_return_output(cmd):
     p = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
     return p.stdout.read()
 
-class Android_Device:
+class Android_Device(Device):
+    '''extended the uiautomator Device class to add a working screenshot method and a save logcat method.
+    als changed the constructor to do all the work of connecting the device to adb
+    to see all the available methods etc of the parent class go here - https://github.com/xiaocong/uiautomator'''
 
     def __init__(self, ip_address):
-        '''connects to android device using ip or serial returns exception on failure'''
+        '''connects to android device using ip or serial returns exception on failure
+        you should also be able to connect using the serial number instead of the IP address
+        '''
+
         self.ip_address = ip_address
         self.device = None
         os.system("adb kill-server > /dev/null 2>&1")
@@ -20,7 +26,7 @@ class Android_Device:
         os.system("adb root > /dev/null 2>&1")
         connection_status = run_cmd_return_output("adb connect " + self.ip_address)
         if  "connected" in run_cmd_return_output("adb connect " + self.ip_address):
-            self.device = Device(ip_address)
+            super(Android_Device, self).__init__()
         else:
             raise Exception(connection_status)
 
@@ -30,26 +36,22 @@ class Android_Device:
         example take_screenshot(bsq, "/images/sample.png")
         uses adb shell cmd instead of uiautomator screenshot
         because it is more reliable - ****warning*** will overwrite files of the same name
-        '''
+        !!!may only run from a mac or some other os where perl exists!!! '''
         ip = self.ip_address
         os.system("adb -s " + ip + " shell screencap -p | perl -pe 's/\\x0D\\x0A/\\x0A/g' > " + png_file )
 
-    def save_logcat_to_file(self, file_name):
+    def save_logcat_to_file(self, file_name, logcat_time=8):
+        '''sends the output of logcat to a file '''
         ip = self.ip_address
         p = subprocess.Popen('adb -s ' + ip +  ' logcat >> ' + file_name, stdout=subprocess.PIPE, shell=True)
-        sleep(8)
+        sleep(logcat_time)
         p.kill()
 
-    def click_view_with_text(self, view_text, wait_time):
-        #wait time is in seconds
-       for i in range(1,wait_time + 1):
-           if self.device(text=view_text).exists:
-               self.device(text=view_text).click()
-               return
-           else:
-               sleep(1)
-       self.take_screenshot("view_not_found.png")
-       raise Exception("view with text " + view_text + " not found")
+
+
+
+
+
 
 
 
--- bsq_action_test.py
+++ bsq_action_test.py
@@ -2,5 +2,7 @@ from android_device import Android_Device
 
 bsq = Android_Device("10.1.108.93")
 bsq.take_screenshot("test.png")
-bsq.save_logcat_to_file("loggy.txt")
-bsq.wait_for_view_and_click("About board", 5)
+bsq.save_logcat_to_file("loggy.txt", 5)
+bsq(text="About board").click()
+bsq.press("back")
+print bsq(text="Sound").info
